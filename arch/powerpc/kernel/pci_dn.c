8dfcbc820dc762f496b82c081a63a472bbe67da9
Sergey Miroshnichenko <s.miroshnichenko@yadro.com>
2019-10-24 16:56:08 +0300

    powerpc/pci: Enable assigning bus numbers instead of reading them from DT
    If the firmware indicates support of reassigning bus numbers via the PHB's
    "ibm,supported-movable-bdfs" property in DT, PowerNV will not depend on PCI
    topology info from DT anymore.
    
    This makes possible to re-enumerate the fabric, assign the new bus numbers
    and switch from the pnv_php module to the standard pciehp driver for PCI
    hotplug functionality.
    
    Signed-off-by: Sergey Miroshnichenko <s.miroshnichenko@yadro.com>

--- arch/powerpc/kernel/pci_dn.c
+++ arch/powerpc/kernel/pci_dn.c
@@ -559,6 +559,11 @@ void pci_devs_phb_init_dynamic(struct pci_controller *phb)
 		phb->pci_data = pdn;
 	}
 
+	if (of_get_property(dn, "ibm,supported-movable-bdfs", NULL)) {
+		pci_add_flags(PCI_REASSIGN_ALL_BUS);
+		return;
+	}
+
 	/* Update dn->phb ptrs for new phb and children devices */
 	pci_traverse_device_nodes(dn, add_pdn, phb);
 }
