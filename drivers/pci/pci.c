2d0896af7ce90305eefe0ae7da6314b1779183cd
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2020-12-18 16:47:27 +0300

    PCI: hotplug: Enable the movable BARs feature by default
    This is the last patch in the series which implements the essentials of the
    movable BARs feature, so it is turned on by default now.
    
    Tested on x86_64; and with extra patches applied, it also works on ppc64le
    (PowerNV).
    
    In case of problems it is still can be overridden by the following command
    line option:
    
      pcie_movable_bars=off
    
    Signed-off-by: Sergei Miroshnichenko <s.miroshnichenko@yadro.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -86,7 +86,7 @@ bool pci_reset_supported(struct pci_dev *dev)
 int pci_domains_supported = 1;
 #endif
 
-bool pci_can_move_bars;
+bool pci_can_move_bars = true;
 
 #define DEFAULT_CARDBUS_IO_SIZE		(256)
 #define DEFAULT_CARDBUS_MEM_SIZE	(64*1024*1024)
